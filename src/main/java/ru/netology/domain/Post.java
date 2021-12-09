56292ad296ff61ae330f6909304d20da5dc25ff9
Natalia Alferova <natalia@alferova.me>
2021-12-09 12:22:27 +0100

    Version 1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    private String avatar;
+    private String name;
+    private String data;
+    private String text;
+    private String imageURL;
+    private String like;
+    private String comment;
+    private String repost;
+    private String viewing;
+}
