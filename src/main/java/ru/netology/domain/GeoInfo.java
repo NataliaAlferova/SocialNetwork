0dd98ef021129492b84e951c5ba9b4853e04097b
Natalia Alferova <natalia@alferova.me>
2021-12-09 14:30:46 +0100

    Version 2

---
+++ src/main/java/ru/netology/domain/GeoInfo.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+public class GeoInfo {
+    private int coordinate;
+    private String country;
+    private String City;
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,13 +1,21 @@
 package ru.netology.domain;
 
 public class Post {
-    private String avatar;
+    private int postID;
+    private int ownerID;
+    private int autorID;
+    private int createdByID;
+    private String avatarURL;
     private String name;
-    private String data;
+    private int data;
     private String text;
     private String imageURL;
-    private String like;
-    private String comment;
-    private String repost;
-    private String viewing;
+    private boolean friendsOnly;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostInfo repostInfo;
+    private GeoInfo geoInfo;
+    private boolean canEdit;
+    private boolean canDelete;
+    private int viewing;
 }
